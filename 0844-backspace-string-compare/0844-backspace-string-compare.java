3342772ad7e8e491f145290de4eb694fcc79fc0d
Prem Burnwal <premburnwal31@gmail.com>
2026-08-05 23:44:13 +0530

    Time: 2 ms (59.71%), Space: 43.6 MB (10.32%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,25 +1,30 @@
 class Solution {
-    public String eval(String s) {
-        Stack<Character> stk = new Stack<>();
-        
-        for (char ch : s.toCharArray()) {
-            if (ch != '#') {
-                stk.push(ch); 
-            } else if (!stk.isEmpty()) {
-                stk.pop();   
-            }
-        }
-        
-        
-        StringBuilder res = new StringBuilder();
-        for (char ch : stk) {
-            res.append(ch);
-        }
-        return res.toString();
-    }
-
     public boolean backspaceCompare(String s, String t) {
-        
-        return eval(s).equals(eval(t));
+        Stack<Character> st = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            // if(!st.isEmpty()) continue;
+            if (s.charAt(i) == '#') {
+                if (!st.isEmpty())
+                    st.pop();
+            } else
+                st.push(s.charAt(i));
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == '#') {
+                if (!st2.isEmpty())
+                    st2.pop();
+            } else
+                st2.push(t.charAt(i));
+        }
+        if (st.size() != st2.size())
+            return false;
+        int len = st.size();
+        for (int i = 0; i < len; i++) {
+            if (st.pop() != st2.pop())
+                return false;
+        }
+        return true;
+
     }
 }
