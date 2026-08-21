5bba09451a01ea168d917879480538d10ac069d4
Prem Burnwal <premburnwal31@gmail.com>
2026-08-21 22:41:28 +0530

    Time: 7 ms (38.8%), Space: 46.3 MB (97.59%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -1,42 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
-
+    HashMap<Integer,Integer> map =new HashMap<>();
     public int[] findMode(TreeNode root) {
         inorder(root);
 
         int max = 0;
 
-        // Find maximum frequency
-        for (int count : map.values()) {
-            max = Math.max(max, count);
+        for(int count: map.values()){
+            max=Math.max(max,count);
         }
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        // Find all values having maximum frequency
-        for (int key : map.keySet()) {
-            if (map.get(key) == max) {
-                list.add(key);
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int count:map.keySet()){
+            if(map.get(count)==max){
+                arr.add(count);
             }
         }
-
-        int[] ans = new int[list.size()];
-
-        for (int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i);
+        int ans[]=new int[arr.size()];
+        for(int i=0;i<arr.size();i++){
+            ans[i]=arr.get(i);
         }
-
         return ans;
+
     }
 
-    public void inorder(TreeNode root) {
-        if (root == null)
-            return;
+
+    public void inorder(TreeNode root){
+        if(root == null) return;
 
         inorder(root.left);
-
-        map.put(root.val, map.getOrDefault(root.val, 0) + 1);
-
-        inorder(root.right);
+        map.put(root.val,map.getOrDefault(root.val,0)+1);
+         inorder(root.right);
     }
 }
